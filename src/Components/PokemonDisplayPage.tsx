e77e4353e67a2f68ae66f80d366045cbc543f727
bobby1072 <75732572+bobby1072@users.noreply.github.com>
2022-10-20 23:27:23 +0100

    added id to pokemon display page

--- src/Components/PokemonDisplayPage.tsx
+++ src/Components/PokemonDisplayPage.tsx
@@ -1,7 +1,6 @@
 import PokemonFull from "../PokeClassLib/PokemonFullClass";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import "./PokemonDisplayType.css";
-import { Stack } from "@mui/material";
 interface IPokemonInfoPropsArgs {
   PokemonObj: PokemonFull;
   goBack: () => void;
@@ -12,19 +11,21 @@ function PokemonInfo(props: IPokemonInfoPropsArgs): JSX.Element {
     <div className="topLevelDiv">
       <div className="DisplayAllPokesDiv">
         <ArrowBackIcon onClick={props.goBack} fontSize="large" />
-        <div className="topLevelDiv">
-          <h1>{Pokemon.PokemonName}</h1>
-        </div>
-        <div className="topLevelDiv">
-          {Pokemon.Types.map((element) => {
-            return (
-              <div className="typeSpacing">
-                <p className={`type-icon type-${element.TypeName}`}>
+        <div className="pokemonDisplayPageDiv">
+          <h1>Name: {Pokemon.PokemonName}</h1>
+          <h2>Id: {Pokemon.Id}</h2>
+          <div className="topLevelDiv">
+            {Pokemon.Types.map((element) => {
+              return (
+                <p
+                  style={{ margin: 1.5 }}
+                  className={`type-icon type-${element.TypeName}`}
+                >
                   {element.TypeName}
                 </p>
-              </div>
-            );
-          })}
+              );
+            })}
+          </div>
         </div>
       </div>
     </div>
