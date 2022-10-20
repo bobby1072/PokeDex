acf8694a5a903aa253c07b2e1b8b97fa928f01a5
bobby1072 <75732572+bobby1072@users.noreply.github.com>
2022-10-20 18:40:15 +0100

    Type displaying works now

--- src/Components/PokemonDisplayPage.tsx
+++ src/Components/PokemonDisplayPage.tsx
@@ -1,16 +1,30 @@
 import PokemonFull from "../PokeClassLib/PokemonFullClass";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import "./PokemonDisplayType.css";
+import { Stack } from "@mui/material";
 interface IPokemonInfoPropsArgs {
   PokemonObj: PokemonFull;
   goBack: () => void;
 }
 function PokemonInfo(props: IPokemonInfoPropsArgs): JSX.Element {
+  const Pokemon: PokemonFull = props.PokemonObj;
   return (
     <div className="topLevelDiv">
       <div className="DisplayAllPokesDiv">
         <ArrowBackIcon onClick={props.goBack} fontSize="large" />
         <div className="topLevelDiv">
-          <p>boi</p>
+          <h1>{Pokemon.PokemonName}</h1>
+        </div>
+        <div className="topLevelDiv">
+          {Pokemon.Types.map((element) => {
+            return (
+              <div className="typeSpacing">
+                <p className={`type-icon type-${element.TypeName}`}>
+                  {element.TypeName}
+                </p>
+              </div>
+            );
+          })}
         </div>
       </div>
     </div>
