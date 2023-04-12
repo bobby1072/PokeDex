5d871f9ee475f9c3e603907347ffc81af477a440
bobby1072 <75732572+bobby1072@users.noreply.github.com>
2023-04-12 09:51:07 +0100

    Electron ready

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import MainPage from "./Components/MainPage";
+import { HashRouter, Route, Routes } from "react-router-dom";
 function App(): JSX.Element {
   React.useEffect(() => {
     document.title = "Pokedex";
   });
   return (
-    <div>
-      <MainPage />
-    </div>
+    <HashRouter>
+      <Routes>
+        <Route element={<MainPage />} path="/" />
+      </Routes>
+    </HashRouter>
   );
 }
 
